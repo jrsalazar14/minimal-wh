52953ae4ac62bdc087b4e8da823e578034e2b9c1
Jose Salazar <jose.salazar@blueriver.com>
2024-03-23 19:21:25 -0500

    Add logic to shown sizes per category

--- api/size/index.ts
+++ api/size/index.ts
@@ -5,7 +5,10 @@ export const useSizeList = () => {
   return useQuery({
     queryKey: ["sizes"],
     queryFn: async () => {
-      const { data, error } = await supabase.from("sizes").select("*");
+      const { data, error } = await supabase
+        .from("sizes")
+        .select("*")
+        .order("id");
       if (error) {
         throw new Error(error.message);
       }
--- app/(tabs)/inventory/[category].tsx
+++ app/(tabs)/inventory/[category].tsx
@@ -1,24 +1,72 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { Stack, useLocalSearchParams } from "expo-router";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Link, Stack, useLocalSearchParams } from "expo-router";
 import { useCategoryList } from "@/api/category";
+import { useInventoryList } from "@/api/inventory";
+import { useSizeList } from "@/api/size";
 
 const CategoryScreen = () => {
   const { category } = useLocalSearchParams();
   const { data: categories } = useCategoryList();
   const cat = categories?.find((c) => c.title.toString() === category);
+  const { data: inventory } = useInventoryList();
+  const { data: sizes } = useSizeList();
+  const [availableSizes, setAvailableSizes] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (cat && inventory && sizes) {
+      const sizesForCategory = inventory
+        .filter((item) => item.category_id === cat.id)
+        .sort((a, b) => a.size_id - b.size_id)
+        .map((item) => item.size_id);
+
+      const orderedSizes = sizesForCategory.map((sizeId) => {
+        const sizeObj = sizes.find((size) => size.id === sizeId);
+        return sizeObj ? sizeObj.size : "";
+      });
+
+      setAvailableSizes(orderedSizes);
+    }
+  }, [cat, inventory, sizes]);
 
   if (!cat) {
     return <Text>Category not found</Text>;
   }
+
   return (
     <View>
       <Stack.Screen options={{ title: cat.title }} />
-      <Text>{cat.title}</Text>
+      <Text style={styles.sizes}>Tallas disponibles:</Text>
+      <View>
+        {availableSizes.map((size, index) => (
+          <Link href={"/(tabs)/inventory/categories/"} asChild>
+            <Pressable>
+              <Text key={index} style={styles.textButton}>
+                {size}
+              </Text>
+            </Pressable>
+          </Link>
+        ))}
+      </View>
     </View>
   );
 };
 
 export default CategoryScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  sizes: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  textButton: {
+    borderRadius: 20,
+    backgroundColor: "#fee782",
+    marginBottom: 10,
+    fontSize: 16,
+    textAlign: "center",
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
